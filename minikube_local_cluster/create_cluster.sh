a222a9c2a946624116c5de06588941a1177f723d
Bruno Luis Cardoso Novo <bruno@Brunos-MacBook-Pro.local>
2022-11-16 20:39:55 -0300

    Adding new ingress files to the project

--- minikube_local_cluster/create_cluster.sh
+++ minikube_local_cluster/create_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# minikube addons enable metrics-server
 
 # minikube addons enable ingress # After the addon is enabled, please run "minikube tunnel" and your ingress resources would be available at "127.0.0.1"
 
@@ -16,3 +17,14 @@ sleep 5
 kubectl -n metallb-system get pods
 
 # minikube tunnel # To start the Ingress Tunnel
+
+# # example.localhost will be resolved as 127.0.0.1, including subdomains
+# # https://hedichaibi.com/how-to-setup-wildcard-dev-domains-with-dnsmasq-on-a-mac/
+# address=/example.localhost/127.0.0.1
+# listen-address=127.0.0.1
+
+# neste arquivo /usr/local/etc/dnsmasq.conf
+
+# sudo services stop dnsmasq
+
+# sudo services start dnsmasq
